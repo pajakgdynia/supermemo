f5e9f07b256f1097e3aed98868a0a9e2e79936fc
Jarek <pajakgdynia@gmail.com>
2018-08-05 10:25:16 +0200

    works with unpacked directories
    This is useful when multiple patch-files were available. Unpacking must be done manually either.

--- parse_course.sh
+++ parse_course.sh
@@ -4,22 +4,35 @@ while [ "$1" != "" ]; do
     F1=$1
     shift
 
-    echo ${F1} | grep '\.smpak$' >/dev/null || continue
+    DIR="`pwd`"
+    if [ -d "${F1}" ]; then
+        cd "${F1}"
+        F=`basename "${F1}"`
+    else
+        if echo ${F1} | grep '\.smpak$' >/dev/null; then
+            true
+        else
+            echo Not a smpak file ${F1} 1>&2
+            continue
+        fi
 
-    F=`basename ${F1} .smpak | sed 's/\.[0-9]*$//1'`
-    echo "${F1} -> ${F}"
+        F=`basename "${F1}" .smpak | sed 's/\.[0-9]*$//1'`
+        echo "${F1} -> ${F}"
 
-    mkdir -p ${F}
-    cd ${F}
-    if [ ! -d items ]; then
-        cp ../${F1} .
-        F2=`ls -1 *.smpak`
-        ../smdec.exe ${F2}
-        ../smunp.exe ${F2}
-        rm ${F2}
-        mkdir items
+        mkdir -p "${F}"
+        if [ ! -d items ]; then
+            cp "${F1}" "${F}"
+            cd "${F}"
+            F2="`ls -1 *.smpak`"
+            ../smdec.exe "${F2}"
+            ../smunp.exe "${F2}"
+            rm "${F2}"
+        else
+            cd "${F}"
+        fi
     fi
 
-    awk -f ../strip_course.awk course.xml | awk -f ../fill_course.awk > ../${F}.def
-    cd ..
+    mkdir -p items
+    awk -f ../strip_course.awk course.xml | awk -f ../fill_course.awk > "${DIR}/${F}.def"
+    cd "${DIR}"
 done
